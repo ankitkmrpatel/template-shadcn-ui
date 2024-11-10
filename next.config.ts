c5d9383c89f31ed1926d6ac9a42f3f08248530fd
ankitkmrpatel <ankitpatel14031@gmail.com>
2024-11-10 23:47:28 +0530

    Added Project FIles

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,88 @@
 import type { NextConfig } from "next";
+import { createContentlayerPlugin } from "next-contentlayer2"
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    turbo: {
+      moduleIdStrategy: 'deterministic',
+      resolveExtensions: [
+        '.mdx',
+        '.tsx',
+        '.ts',
+        '.jsx',
+        '.js',
+        '.mjs',
+        '.json',
+      ],
+    },
+  },
+  reactStrictMode: true,
+  // swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
+  // redirects() {
+  //   return [
+  //     {
+  //       source: "/components",
+  //       destination: "/docs/components/accordion",
+  //       permanent: true,
+  //     },
+  //     {
+  //       source: "/docs/components",
+  //       destination: "/docs/components/accordion",
+  //       permanent: true,
+  //     },
+  //     {
+  //       source: "/examples",
+  //       destination: "/examples/mail",
+  //       permanent: false,
+  //     },
+  //     {
+  //       source: "/docs/primitives/:path*",
+  //       destination: "/docs/components/:path*",
+  //       permanent: true,
+  //     },
+  //     {
+  //       source: "/figma",
+  //       destination: "/docs/figma",
+  //       permanent: true,
+  //     },
+  //     {
+  //       source: "/docs/forms",
+  //       destination: "/docs/components/form",
+  //       permanent: false,
+  //     },
+  //     {
+  //       source: "/docs/forms/react-hook-form",
+  //       destination: "/docs/components/form",
+  //       permanent: false,
+  //     },
+  //     {
+  //       source: "/sidebar",
+  //       destination: "/docs/components/sidebar",
+  //       permanent: true,
+  //     },
+  //     {
+  //       source: "/react-19",
+  //       destination: "/docs/react-19",
+  //       permanent: true,
+  //     },
+  //   ]
+  // },
 };
 
-export default nextConfig;
+const withContentlayer = createContentlayerPlugin({
+  // Additional Contentlayer config options
+})
+
+export default withContentlayer(nextConfig)
